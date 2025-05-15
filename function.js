5451034377b0e52006fec2a27a1b29368605d83b
Roygicheru <gicheru.k.roy@gmail.com>
2025-05-15 14:27:10 +0300

    new change

--- function.js
+++ function.js
@@ -1,1 +1,19 @@
 // Functions
+/* A function is an independent block of code that does a task*/
+function greet() {
+    console.log("Hello World!");
+}
+
+// store a function in the displayPI variable
+// this is a function expression
+let displayPI = function() {
+    console.log("PI = 3.14");
+}
+
+// call the greet() function
+greet();
+
+// call the reply() function
+displayPI();
+
+// JS Function arguments
