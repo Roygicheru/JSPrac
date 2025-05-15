1a37390e150be5ae2753e1390eb831d3a8f4ee07
Roygicheru <gicheru.k.roy@gmail.com>
2025-05-15 15:12:34 +0300

    new change

--- function.js
+++ function.js
@@ -17,3 +17,31 @@ greet();
 displayPI();
 
 // JS Function arguments
+
+// function with a parameter called name
+function greetOne (name){
+    console.log("Hello " + name)
+}
+
+greetOne("Tim")
+
+function addNumbers(num1, num2) {
+    let sum = num1 + num2;
+    console.log(`Sum: ${sum}`);
+}
+
+addNumbers(5, 4);
+
+// The return Statement
+// We can return a value from a JavaScript function using the return statement.
+
+// function to find square of a number
+function findSquare(num) {
+    // return square
+    return num * num; 
+}
+
+// call the function and store the result
+let square = findSquare(3);
+
+console.log(`Square: ${square}`);
